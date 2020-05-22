86e4567e08e818b3f1bd09eb7c20ec4027784feb
songjie <364063862@qq.com>
2020-05-22 23:54:12 +0800

    netty server

--- src/main/java/nettys/NettyServer.java
+++ src/main/java/nettys/NettyServer.java
@@ -1,8 +1,13 @@
 package nettys;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class NettyServer {
     public static void main(String [] args){
@@ -10,7 +15,18 @@ public class NettyServer {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workeGroup = new NioEventLoopGroup();
 
+        //创建服务端的自动对像，配置参数
         ServerBootstrap bootstrap = new ServerBootstrap();
 
+        bootstrap.group(bossGroup,workeGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG,128)  //设置线程队列的连接个数
+                .childOption(ChannelOption.SO_KEEPALIVE,true)//设置保持活动连接状态
+                .childHandler(new ChannelInitializer<SocketChannel>() { //创建一个通道测试对像
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(null);
+                    }
+                });
     }
 }
